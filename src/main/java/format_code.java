aefdffd958c04d633e50c5225870f435e2aff93e
Danil Olizko <olizko-omsk@mail.ru>
2017-10-30 16:05:09 +0600

    FirstFormatting

---
+++ src/main/java/format_code.java
@@ -0,0 +1,62 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class format_code {
+        public static String[] splitStr(final String splitString,final int stringIndex) {
+            char[] firstStr = new char[stringIndex];
+            char[] secondStr = new char[splitString.length() - stringIndex];
+            String[] result = new String[2];
+
+            char[] charArr = splitString.toCharArray();
+
+            for (int i = 0; i < stringIndex; i++) {
+                firstStr[i] = charArr[i];
+            }
+            int k = 0;
+            for (int i = stringIndex; i < splitString.length(); i++) {
+                secondStr[k] = charArr[i];
+                k++;
+            }
+            result[0] = new String(firstStr);
+            result[1] = new String(secondStr);
+            return result;
+
+        }
+
+        public static void main(final String[] args) throws IOException {
+            FileReader fr = new FileReader("1.txt");
+            String str = "";
+            int c;
+            while ((c = fr.read()) != -1) {
+                str += (char) c;
+            }
+
+            int countOpen = 0;
+            int index = 0;
+            String space = "";
+            String enter = "\n";
+            for (int i = 0; i <str.length() ; i++) {
+                index++;
+                if (str.charAt(i) == '{') {
+                    countOpen++;
+                    space += "    ";
+                    String[] strings = splitStr(str, index);
+                    str = strings[0] + enter + space + strings[1];
+                }
+                if (str.charAt(i) == ';') {
+                    String[] strings = splitStr(str, index);
+                    str = strings[0] + enter + space + strings[1];
+                }
+                if (str.charAt(i) == '}') {
+                    String[] strings = splitStr(str, index);
+                    str = strings[0] + enter + space + strings[1];
+                }
+                if (str.charAt(i) == '}') {
+                    String[] spaces = splitStr(space, countOpen * 4);
+                    space = spaces[0];
+                    countOpen--;
+                }
+            }
+            System.out.println(str);
+        }
+    }
